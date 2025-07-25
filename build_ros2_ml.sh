98de3d39cbd824bdfe2e4e333025a5109a1de2ef
Austin Gregg-Smith <blooop@gmail.com>
2025-07-25 15:30:18 +0100

    fix path

--- build_ros2_ml.sh
+++ build_ros2_ml.sh
@@ -14,4 +14,5 @@ docker buildx build --push \
     --build-arg TF_VERSION="2.18.0" \
     --build-arg ONNX_RUNTIME_VERSION="1.20.1" \
     --build-arg TRITON_VERSION="2.52.0" \
-    --tag kinisirobotics/ros2-ml:jazzy-desktop-full-tf2.18.0-torch2.5.0-v25.07
+    --tag kinisirobotics/ros2-ml:jazzy-desktop-full-tf2.18.0-torch2.5.0-v25.07 \
+    .
